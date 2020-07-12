39993f5aa15c7bc4c9a6a4f2a4ac8d6e208a5b98
rashzero <rashzero0@gmail.com>
2020-07-12 14:06:30 +0300

    Updates

---
+++ precache-manifest.dcafe5b0c6c982625b3cfdc585d1141c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0db4f473800a6c4fd4b8d9bc2ee857f",
+    "url": "/yarandin-test/index.html"
+  },
+  {
+    "revision": "3a6de1f67e49a5d26f36",
+    "url": "/yarandin-test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8d951ae812d11b2057a6",
+    "url": "/yarandin-test/static/js/2.3a04cbe8.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/yarandin-test/static/js/2.3a04cbe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a6de1f67e49a5d26f36",
+    "url": "/yarandin-test/static/js/main.fa625c23.chunk.js"
+  },
+  {
+    "revision": "27817ba1ba4ef3e8febb",
+    "url": "/yarandin-test/static/js/runtime-main.4b606cdc.js"
+  }
+]);
